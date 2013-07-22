0db5dc03f0d5e1542b9ce8e56aa1afbbba5f2469
arvand23 <aalviri@gmail.com>
2013-07-22 13:04:41 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cakewalk::Application.config.secret_token = '229efc38be1b4c114c28a883d2bb1c0602f202230b5714a804bd5f3578fe4787f92775d41027175e24f82b6802e5e3786668f081c5d6e9bcdc8f844e05796dd8'
